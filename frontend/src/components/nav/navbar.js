41499fa43483649d17efc029e23c9195b517fac0
Luat Pham <luat32@gmail.com>
2019-09-19 18:07:43 -0500

    Added css styling to nav bar

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -8,8 +8,31 @@ class NavBar extends React.Component {
     super(props);
     this.logoutUser = this.logoutUser.bind(this);
     this.getLinks = this.getLinks.bind(this);
+    this.state = { dropdown: 'dropdown-hidden' }
+    this.toggleDropdown = this.toggleDropdown.bind(this);
   }
-
+  toggleDropdown() {
+    if (this.state.dropdown === 'dropdown-hidden') {
+      this.setState({ dropdown: 'dropdown-visible' });
+    } else {
+      this.setState({ dropdown: 'dropdown-hidden' });
+    }
+  }
+  handleClick(e) {
+    if (this.dropdownRef && (this.dropdownRef.contains(e.target) || this.iconRef.contains(e.target))) {
+      return
+    }
+    this.setState({ dropdown: 'dropdown-hidden' })
+  }
+  handleKeyUp(e) {
+    if (e.keyCode === 13) {
+      if (this.state.dropdown === 'dropdown-hidden') {
+        this.setState({ dropdown: 'dropdown-visible' });
+      } else {
+        this.setState({ dropdown: 'dropdown-hidden' });
+      }
+    }
+  }
   logoutUser(e) {
     e.preventDefault();
     this.props.logout();
@@ -18,19 +41,16 @@ class NavBar extends React.Component {
   // Selectively render links dependent on whether the user is logged in
   getLinks() {
     let createLink;
-
     let onePageLink = this.props.onePage ? (
       <Link to={`/onePages/${this.props.onePage._id}`}>Your One-Page</Link>
     ) : (
       <Link to={`/onePages/new`}>Create a One-Page</Link>
     )
-    
     let resumeLink = this.props.resume ? (
       <Link to={`/resumes/${this.props.resume._id}`}>Your Resume</Link>
       ) : (
         <Link to={'/resumes/new'}>Create A Resume</Link>
         )
-
     if (this.props.user && this.props.user.role === "Employer"){
       createLink = onePageLink;
     } else {
@@ -39,15 +59,29 @@ class NavBar extends React.Component {
     if (this.props.loggedIn) {
       return (
         <div className="nav-bar-user">
-          <button className="button-1">My Profile</button>
-          <button className="button-2">My Matches</button>
-          <Link to={'/home'}>All Results/Feed</Link>
-          <Link to={'/profile'}>Profile</Link>
-          <Link to={'/preferences'}>Update your preferences</Link>
-          <i className="down"></i>
-          {/* <Link to={'/resumes/new'}>Create A Resume</Link> */}
-          {createLink}
-          <button onClick={this.logoutUser}>Logout</button>
+          <Link to={`/users/${this.props.user.id}/profile`}>
+            <button className="session-btn my-profile-btn">My Profile</button>
+          </Link>
+          <button className="session-btn my-matches-btn">My Matches</button>
+          <i className="down" 
+            onClick={this.toggleDropdown} 
+            onKeyUp={this.handleKeyUp}></i>
+          <ul id='dropdown' className={this.state.dropdown}>
+            <span className="dropdown-items">
+              <li className="drop-list-item">
+                <Link to={'/home'}>All Results/Feed</Link></li>
+              <li className="drop-list-item">
+                <Link to={'/profile'}>Profile</Link></li>
+              <li className="drop-list-item">
+                <Link to={'/preferences'}>Update your preferences</Link></li>
+              <li className="drop-list-item">
+                {createLink}</li>
+              <li className="drop-list-item">
+                <button className="btn">Empty Button</button></li>
+              <li className="drop-list-item">
+                <button onClick={this.logoutUser}>Logout</button></li>
+            </span>
+          </ul>
         </div>
       );
     } else {

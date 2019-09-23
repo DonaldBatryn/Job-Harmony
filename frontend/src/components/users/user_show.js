6149790545c3f07b0fe7abe7b8636c421d39bc37
Donnie <donald.batryn@gmail.com>
2019-09-23 16:19:31 -0500

    user profile basics

--- frontend/src/components/users/user_show.js
+++ frontend/src/components/users/user_show.js
@@ -6,7 +6,17 @@ class UserShow extends React.Component{
     render(){
         return (
             <div className="user-show-container">
+                <div className="user-show-top">
+                    <div className="user-avatar">
+                        <img className="user-avatar-img" src="" alt=""/>
+                    </div>
+                    <div className="user-show-links">
 
+                    </div>
+                </div>
+                <div className="user-show-bottom">
+
+                </div>
             </div>
         )
     }

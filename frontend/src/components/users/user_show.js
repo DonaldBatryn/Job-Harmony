f259ceeacf3ef241da9d1940fff7251cef0fa30e
Donnie <donald.batryn@gmail.com>
2019-09-23 16:54:30 -0500

    user profile with link to resume

--- frontend/src/components/users/user_show.js
+++ frontend/src/components/users/user_show.js
@@ -1,16 +1,21 @@
 import React from 'react'
+import avatar from '../../images/avatar.png'
+import { Link } from 'react-router-dom'
 
 class UserShow extends React.Component{
 
 
     render(){
+        let { user, resume } = this.props;
         return (
             <div className="user-show-container">
                 <div className="user-show-top">
                     <div className="user-avatar">
-                        <img className="user-avatar-img" src="" alt=""/>
+                        <img className="user-avatar-img" src={avatar} alt=""/>
+                        <h4>{user.fName}&nbsp;{user.lName}</h4>
                     </div>
                     <div className="user-show-links">
+                        <h2><Link to={`/resumes/${resume._id}`}>Your Resume</Link></h2>
 
                     </div>
                 </div>

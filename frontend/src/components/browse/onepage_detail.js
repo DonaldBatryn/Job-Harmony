3790d0b860b479fdd3345f6ac269ccb57ac350de
Donnie <donald.batryn@gmail.com>
2019-10-29 22:33:25 -0500

    restyled browse show and made responsive

--- frontend/src/components/browse/onepage_detail.js
+++ frontend/src/components/browse/onepage_detail.js
@@ -18,39 +18,80 @@ const OnePageDetail = ({ onePage}) => {
     remoteValue = "No"
   }
   return (
+    // <div className="onepage-detail-container">
+    //   <div className="op-detail-header">
+    //     <div className="op-header-pic">
+    //       <div className={`banner ${jobField}-${randomNum}`}>
+    //       </div>
+    //       <h3>{catchPhrase}</h3>
+    //       {/* <img src={onePage.image} alt=""/> */}
+    //     </div>
+    //   </div>
+    //   <div className="op-detail-bottom-box">
+    //     <div className="op-header-text">
+    //     <h1>{jobTitle}</h1>
+
+    //       <h2>at&nbsp;{onePage.companyName}</h2>
+    //     </div>
+    //     <div className="op-detail-left">
+    //       <h3>Starting Salary:&nbsp;${onePage.startingPay}</h3>
+    //       <h3>Job Description:&nbsp;</h3>
+    //       <p>{onePage.description}</p>
+          
+          
+    //       <h4>Field:&nbsp;{jobField}</h4>
+          
+        
+    //     </div>
+        // <div className="op-detail-right">
+        //   <h4>Skills Required:&nbsp;</h4>
+        //   <ul>{goodJobSkills}</ul>
+          
+          
+        //   <h5>Position is:&nbsp;{onePage.type}</h5>
+        //   <h5>Remote?:&nbsp;{remoteValue}</h5>
+        //   <h5>Benefits:&nbsp;{onePage.benefits}</h5>
+        // </div>
+    //   </div>
+    // </div>
     <div className="onepage-detail-container">
       <div className="op-detail-header">
-        <div className="op-header-pic">
-          <div className={`banner ${jobField}-${randomNum}`}>
-          </div>
-          <h3>{catchPhrase}</h3>
-          {/* <img src={onePage.image} alt=""/> */}
+        <div className={`banner ${jobField}-${randomNum}`}>
+        </div>
+         <div className="op-header-text">
+          <h1>{jobTitle}</h1>
+          <h2>at&nbsp;{onePage.companyName}</h2>
+          <h4>Field:&nbsp;{jobField}</h4>
         </div>
       </div>
       <div className="op-detail-bottom-box">
-        <div className="op-header-text">
-        <h1>{jobTitle}</h1>
-
-          <h2>at&nbsp;{onePage.companyName}</h2>
-        </div>
         <div className="op-detail-left">
-          <h3>Starting Salary:&nbsp;${onePage.startingPay}</h3>
-          <h3>Job Description:&nbsp;</h3>
-          <p>{onePage.description}</p>
-          
-          
-          <h4>Field:&nbsp;{jobField}</h4>
-          
-        
+          <div className="salary-div">
+            <h3>Starting Salary:</h3>
+            <h3>${onePage.startingPay}</h3>
+          </div>
+          <div className="job-descrip-div">
+            <h3>Job Description:</h3>
+            <p>{onePage.description}</p>
+          </div>
+          <div className="skills-req-div">
+            <h3>Skills Required</h3>
+            <ul>{goodJobSkills}</ul>
+          </div>
         </div>
         <div className="op-detail-right">
-          <h4>Skills Required:&nbsp;</h4>
-          <ul>{goodJobSkills}</ul>
-          
-          
-          <h5>Position is:&nbsp;{onePage.type}</h5>
-          <h5>Remote?:&nbsp;{remoteValue}</h5>
-          <h5>Benefits:&nbsp;{onePage.benefits}</h5>
+          <div className="position-div">
+            <h3>Position is:</h3>
+            <h3>{onePage.type}</h3>
+          </div>
+          <div className="remote-div">
+            <h3>Remote?:</h3>
+            <h3>{remoteValue}</h3>
+          </div>
+          <div className="benefits-div">
+            <h3>Benefits</h3>
+            <p>{onePage.benefits}</p>
+          </div>
         </div>
       </div>
     </div>

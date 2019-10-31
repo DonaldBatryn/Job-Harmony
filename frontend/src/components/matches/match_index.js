303d83f74009ebef87dbb14e8753206803684b9d
Donnie <donald.batryn@gmail.com>
2019-10-31 00:21:11 -0500

    added a matches landing page

---
+++ frontend/src/components/matches/match_index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MatchIndex extends React.Component {
+
+
+    render(){
+        return (
+            <div className="matches-index-container">
+                <h2>You dont have any matches yet.</h2>
+                <h4>Connect with jobs you like and check back later.</h4>
+            </div>
+        )
+    }
+}
+
+export default MatchIndex;

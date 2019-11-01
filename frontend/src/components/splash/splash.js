82687501c7e9d19e89a0168dd3f9d24ff4f7f0f9
Donnie <donald.batryn@gmail.com>
2019-10-31 22:02:01 -0500

    added splash pages

--- frontend/src/components/splash/splash.js
+++ frontend/src/components/splash/splash.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import createResume from '../../images/createResume.png'
+import browseJobs from '../../images/browseJobs.png'
+import findMatches from '../../images/findMatches.png'
 
 class Splash extends React.Component{
     constructor(props){
@@ -45,22 +50,46 @@ class Splash extends React.Component{
             this.setState({ slashClassThree: 'scroll-down' });
         }
     }
+
+    openModalFor(form) {
+        return e => {
+            this.props.openModal(form)
+        }
+    }
+
     render(){
         return (
             <div className="splash-container">
+                    <img className="hero" src="https://harmonyd.com/wp-content/uploads/2018/05/18-HD-Perfect-Harmony-Dark-Flip.jpg" alt=""/>
                 <div className={`splash-1 ${this.state.slashClassOne}`} onWheel={this.handleWheelOne}>
                     <h1 className="splash-title">Job-Harmony</h1>
                     <div className="intro-text">
                         <h2>Your career will thank you.</h2>
                     </div>
                     <div>
-                    <img className="hero" src="https://harmonyd.com/wp-content/uploads/2018/05/18-HD-Perfect-Harmony-Dark-Flip.jpg" alt=""/>
                     </div>
                     {/* <div className="slider"></div> */}
                 </div>
-                <div className={`splash-2 ${this.state.slashClassTwo}`} onWheel={this.handleWheelTwo}>slide 2</div>
-                <div className={`splash-3 ${this.state.slashClassThree}`} onWheel={this.handleWheelThree}>slide 3</div>
-                <div className={`splash-4 ${this.state.slashClassFour}`} onWheel={this.handleWheelFour}>slide 4</div>
+                <div className={`splash-2 ${this.state.slashClassTwo}`} onWheel={this.handleWheelTwo}>
+                    <h1 className="intro-text exp-text">Create a Resume</h1>
+                    <img className="splash-image" src={createResume} />
+                </div>
+                <div className={`splash-3 ${this.state.slashClassThree}`} onWheel={this.handleWheelThree}>
+                    <h1 className="intro-text exp-text">Browse for jobs that match your skills</h1>
+                    <img className="splash-image" src={browseJobs} />
+                </div>
+                <div className={`splash-4 ${this.state.slashClassFour}`} onWheel={this.handleWheelFour}>
+                    <h1 className="intro-text exp-text">Find your perfect match</h1>
+                    <img className="splash-image" src={findMatches} />
+                    <div className="splash-auth-buttons">
+                        <button className="session-btn auth-btn" onClick={this.openModalFor('signup')}>
+                            <Link className="my-profile-btn" to={'/signup'}>Signup</Link>
+                        </button>
+                        <button className="session-btn log-in-nav" onClick={this.openModalFor('login')}>
+                            Login
+                        </button>
+                    </div>
+                </div>
             </div>
         )
     }
--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -23,7 +23,7 @@ const Protected = ({ component: Component, loggedIn, ...rest }) => (
         <Component {...props} />
       ) : (
           // Redirect to the login page if the user is already authenticated
-          <Redirect to="/login" />
+          <Redirect to="/" />
         )
     }
   />
